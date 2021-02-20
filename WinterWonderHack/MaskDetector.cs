be54f8c4a903b4f16dd2e2e86444c62bed3f5558
Baker <cowwaver@gmail.com>
2021-02-19 21:36:51 -0700

    f

--- WinterWonderHack/MaskDetector.cs
+++ WinterWonderHack/MaskDetector.cs
@@ -12,17 +12,18 @@ namespace WinterWonderHack
         public void Run()
         {
             Mat x = new Mat("../../../Pictures/default0.png", ImreadModes.Grayscale);
-
-            
+          
+           
             Cv2.NamedWindow("Bobby", WindowFlags.Normal);
             Cv2.ImShow("Bobby", x);
             Cv2.StartWindowThread();
 
             ImageEncodingParam param = new ImageEncodingParam(ImwriteFlags.PngStrategy, (int)ImwritePNGFlags.StrategyDefault);
             x.SaveImage("../../../Output/output1.png", new ImageEncodingParam[] {param});
+            x.Canny(x, x, 0, 300, 3);
 
 
-            
+
             //Cv2.ImShow()
         }
     }
