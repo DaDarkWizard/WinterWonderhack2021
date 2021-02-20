fd09e6bc0f7a0a6318459c9aceab711b0405c3bf
DaDarkWizard <danimask@ilc.edu>
2021-02-19 23:23:44 -0500

    Yeet

--- WinterWonderHack/MaskDetector.cs
+++ WinterWonderHack/MaskDetector.cs
@@ -13,9 +13,16 @@ namespace WinterWonderHack
         {
             Mat x = new Mat("../../../Pictures/default0.png", ImreadModes.Grayscale);
 
+            Cv2.ImShow("Bobby", x);
+            Cv2.NamedWindow("Bobby", WindowFlags.Normal);
+            
+
             ImageEncodingParam param = new ImageEncodingParam(ImwriteFlags.PngStrategy, (int)ImwritePNGFlags.StrategyDefault);
             x.SaveImage("../../../Output/output1.png", new ImageEncodingParam[] {param});
 
+
+            
+            //Cv2.ImShow()
         }
     }
 }
