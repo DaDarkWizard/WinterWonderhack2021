5758e445f3f7928fa56c47d7ae39262e67f54e24
Baker <cowwaver@gmail.com>
2021-02-19 22:25:25 -0700

    Loop through all

--- WinterWonderHack/MaskDetector.cs
+++ WinterWonderHack/MaskDetector.cs
@@ -10,27 +10,29 @@ namespace WinterWonderHack
     class MaskDetector
     {
         public void Run()
-        {
-            Mat rawImage = new Mat("../../../Pictures/default0.png", ImreadModes.Grayscale);
-            rawImage.Line(0, 0, 10, 10, new Scalar { Val0 = 10, Val1 = 10, Val2 = 10, Val3 = 10 });
-            
-            Size duck = new Size(5, 5);
-            
-            Cv2.Canny(rawImage, rawImage, 150, 245);
-            Cv2.GaussianBlur(rawImage, rawImage, duck,0,0);
-            Cv2.NamedWindow("Bobby", WindowFlags.Normal);
-            Cv2.ImShow("Bobby", rawImage);
-            Cv2.StartWindowThread();
-            while (true)
+        {            
+            for (int i = 0; i< 8; i++)
             {
-                int key = Cv2.WaitKey();
-                if (key == 27)
-                    break;
-            }
-            
-            ImageEncodingParam param = new ImageEncodingParam(ImwriteFlags.PngStrategy, (int)ImwritePNGFlags.StrategyDefault);
-            rawImage.SaveImage("../../../Output/output1.png", new ImageEncodingParam[] {param});
+                Mat rawImage = new Mat("../../../Pictures/default" + i + ".png", ImreadModes.Grayscale);
+                rawImage.Line(0, 0, 10, 10, new Scalar { Val0 = 10, Val1 = 10, Val2 = 10, Val3 = 10 });
 
+                Size duck = new Size(7, 7);
+
+                Cv2.Canny(rawImage, rawImage, 150, 245);
+                Cv2.GaussianBlur(rawImage, rawImage, duck, 0, 0);
+                Cv2.NamedWindow("Bobby", WindowFlags.Normal);
+                Cv2.ImShow("Bobby", rawImage);
+                Cv2.StartWindowThread();
+                while (true)
+                {
+                    int key = Cv2.WaitKey();
+                    if (key == 27)
+                        break;
+                }
+
+                ImageEncodingParam param = new ImageEncodingParam(ImwriteFlags.PngStrategy, (int)ImwritePNGFlags.StrategyDefault);
+                rawImage.SaveImage("../../../Output/output1.png", new ImageEncodingParam[] { param });
+            }
             
             
             //Cv2.ImShow()
